6d8d9af3180c20ca3b69a6f393bced46311f5e87
dgavrichkov <dgavrichkiv32@gmail.con>
2026-05-20 22:33:34 +0400

    refactor: добавлена обертка для fetch

--- src/entities/character/repository/character.api.repository.ts
+++ src/entities/character/repository/character.api.repository.ts
@@ -1,31 +1,26 @@
 import type { CharacterRepository } from "./character.port";
 import type { CharacterAPIResponse } from "../character.interface";
-
-const ENDPOINT = "https://rickandmortyapi.com/api/character";
+import { httpClient } from "@/shared/api/httpClient";
 
 export class CharacterApiRepository implements CharacterRepository {
   async getCharactersByName(name: string): Promise<CharacterAPIResponse> {
-    const response = await fetch(
-      `${ENDPOINT}/?name=${encodeURIComponent(name)}`,
+    const response = await httpClient.get(
+      `character/?name=${encodeURIComponent(name)}`,
     );
 
-    if (!response.ok) {
-      // репозиторий решает, что для этого эндпоинта значит 404. Здесь это "персонаж не найден", значит можно нормализовать ответ, а не бросать ошибку
-      if (response.status === 404) {
-        return {
-          results: [],
-          info: {
-            count: 0,
-            pages: 0,
-            next: null,
-            prev: null,
-          },
-        };
-      } else {
-        throw new Error(`Request failed: ${response.status}`);
-      }
+    // репозиторий решает, что для этого эндпоинта значит 404. Здесь это "персонаж не найден", значит можно нормализовать ответ, а не бросать ошибку
+    if (response.status === 404) {
+      return {
+        results: [],
+        info: {
+          count: 0,
+          pages: 0,
+          next: null,
+          prev: null,
+        },
+      };
     }
 
-    return response.json();
+    return response.data as CharacterAPIResponse;
   }
 }
